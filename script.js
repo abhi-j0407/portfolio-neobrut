5f8218e765285577601470d982f9cb2e0deb86b6
Abhishek Jain <abhishek.j0407@gmail.com>
2023-06-25 11:37:54 +0530

    Intersectional observer added, contact section working, p tag issues fixed

---
+++ script.js
@@ -0,0 +1,22 @@
+// Intersectional observer
+const workEx = document.querySelectorAll('.company-container')
+const projects = document.querySelectorAll('.project')
+
+const observer = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            entry.target.classList.toggle("highlight", entry.isIntersecting)
+        })
+    },
+    {
+        threshold:0.75
+    }
+)
+
+workEx.forEach(workExp => {
+    observer.observe(workExp)
+})
+
+projects.forEach(project => {
+    observer.observe(project)
+})
