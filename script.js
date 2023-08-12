155a9f31f74116c5483a4ace6da786e02cc8f2f7
Abhishek Jain <abhishek.j0407@gmail.com>
2023-08-12 11:58:09 +0530

    Hacker text effect on name

--- script.js
+++ script.js
@@ -20,3 +20,42 @@ workEx.forEach(workExp => {
 projects.forEach(project => {
     observer.observe(project)
 })
+
+
+
+//Hacker text animation
+const letters = "abcdefghijklmnopqrstuvwxyz";
+
+let interval = null;
+
+const hackElements = document.getElementsByClassName('hacker-text');
+console.log(hackElements)
+
+const hackerFunc = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
+}
+
+Array.from(hackElements).forEach(element => {
+    element.addEventListener('mouseover', hackerFunc);
+});
